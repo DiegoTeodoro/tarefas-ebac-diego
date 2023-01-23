985473e5e0eb30458ad3bac2aa95384ae2257197
DiegoTeodoro <diegoteodoro_alves@yahoo.com.br>
2023-01-23 09:35:13 -0300

    Projeto criado

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        double a = 10.0;
+        double b  = 3.0;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        
+    }
+}
